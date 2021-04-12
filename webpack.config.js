837f46bf6267a13444df12f6f09438934591517b
David <davidpavlovicdev@gmail.com>
2021-04-12 14:58:12 +0200

    Add copy plugin for images

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
+const path = require("path");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 let mode = "development";
+// Target is quick fix for webpack 5 because hotreload have have currently bug
 let target = "web";
 
 if(process.env.NODE_ENV === "production") {
@@ -10,18 +13,31 @@ if(process.env.NODE_ENV === "production") {
 module.exports = {
     mode: mode,
     target: target,
+    entry: "./src/index.js",
+
+    output: {
+      // this places all images processed in an image folder
+      assetModuleFilename: "images/[hash][ext][query]",
+    },
 
     module: {
         rules: [
             {
                 test: /\.(s[ac]|c)ss$/i,
                 use: [
-                    MiniCssExtractPlugin.loader,
+                   {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: { publicPath: "" },
+                   },
                     "css-loader",
                     "postcss-loader",
                     "sass-loader"
                 ],
             },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: "asset",
+              },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
@@ -32,7 +48,14 @@ module.exports = {
         ]
     },
 
-    plugins: [new MiniCssExtractPlugin()],
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new CopyPlugin({
+            patterns: [
+              { from: "src/images", to: "images" },
+            ],
+          }),
+    ],
 
     // options (false - display unminified version)
     devtool: "source-map",
